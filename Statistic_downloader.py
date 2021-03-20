eb3568a77a724745867798e59e3322a49c7e68ef
Lukas Hennies Laptop <lukas.hennies@rwth-aachen.de>
2021-03-20 04:51:33 +0100

    added fix for case-fatality-rate error

--- Statistic_downloader.py
+++ Statistic_downloader.py
@@ -116,7 +116,13 @@ class DataPoint:
         elif self.headers == ['FIPS', 'Admin2', 'Province_State', 'Country_Region', 'Last_Update', 'Lat', 'Long_',
                               'Confirmed', 'Deaths', 'Recovered', 'Active', 'Combined_Key', 'Incidence_Rate',
                               'Case-Fatality_Ratio']:
+
             self.fill_from_header5()
+        elif self.headers == ['FIPS', 'Admin2', 'Province_State', 'Country_Region', 'Last_Update', 'Lat', 'Long_',
+            'Confirmed',
+            'Deaths', 'Recovered', 'Active', 'Combined_Key', 'Incident_Rate',
+            'Case_Fatality_Ratio']:
+            self.fill_from_header6()
         else:
             print('USING DEBUG METHOD', self.headers)
             self.fill_from_header5()
@@ -254,6 +260,54 @@ class DataPoint:
                 'Province_State': self.in_list[2],
                 'Country_Region': self.in_list[3],
 
+
+
+                'Combined_Key': self.in_list[11]
+            }
+            self.fields = {
+                'Last_Update': fix_american_dates(self.in_list[4]),
+                'Lat': safe_float_cast(self.in_list[5]),
+                'Long_': safe_float_cast(self.in_list[6]),
+                'Incident_Rate': safe_float_cast(self.in_list[12]),
+                'Case-Fatality_Ratio': safe_float_cast(self.in_list[13])
+            }
+
+            to_cast_to_int = [('Deaths', self.in_list[8]),
+                              ('Recovered', self.in_list[9]),
+                              ('Active', self.in_list[10]),
+                              ('Confirmed', self.in_list[7])]
+            for p in to_cast_to_int:
+                try:
+                    self.fields[p[0]] = int(p[1])
+                except Exception as _e:
+                    print("{} can not Format {} from line {}".format(_e, p, self.in_list))
+
+
+        except ValueError as _v:
+            print(_v, self.in_list)
+    def fill_from_header6(self):
+        requiered_headers = ['FIPS', 'Admin2', 'Province_State', 'Country_Region', 'Last_Update', 'Lat', 'Long_',
+                             'Confirmed', 'Deaths', 'Recovered', 'Active', 'Combined_Key', 'Incidence_Rate',
+                             'Case_Fatality_Ratio']
+        for i in range(len(requiered_headers)):
+            try:
+                if requiered_headers[i] != self.headers[i]:
+                    raise HeaderMismatch("Header Mismatch fix headers or DataPoint.fill_from_header6")
+            except IndexError as ind:
+                print(ind, ind.args)
+                raise HeaderMismatch("Header Mismatch fix headers or DataPoint.fill_from_header6")
+            except ValueError as val:
+                print(val, val.args)
+                raise HeaderMismatch("Header Mismatch fix headers or DataPoint.fill_from_header6")
+        try:
+            self.measurement = 'cases'
+            self.time = set_time_to_noon(self.in_list[4])
+            self.tags = {
+                'FIPS': self.in_list[0],
+                'Admin2': self.in_list[1],
+                'Province_State': self.in_list[2],
+                'Country_Region': self.in_list[3],
+
                 # Todo cast to datetime object => new function to transform all string formats..
 
                 'Combined_Key': self.in_list[11]
@@ -274,7 +328,7 @@ class DataPoint:
                 try:
                     self.fields[p[0]] = int(p[1])
                 except Exception as _e:
-                    print("{} can not Format {} from line {}".format(_e, p,self.in_list))
+                    print("{} can not Format {} from line {}".format(_e, p, self.in_list))
 
 
         except ValueError as _v:
@@ -328,7 +382,7 @@ def get_timeseries_points():
 
     git = Github(GITHUB_CONFIG["auth_token"])
     repository = git.get_repo('CSSEGISandData/COVID-19')
-    contents = repository.get_contents("csse_covid_19_data/csse_covid_19_time_series/")
+    contents = repository.get_contents("csse_covid_19_data/csse_covid_19_time_series")
     deaths = ''
     confirmed = ''
     recovered = ''
@@ -476,7 +530,7 @@ def get_data(github_link):
 
 
 def get_provinces():
-    province_location = "csse_covid_19_data/"
+    province_location = "csse_covid_19_data"
     git = Github(GITHUB_CONFIG["auth_token"])
     repository = git.get_repo('CSSEGISandData/COVID-19')
     contents = repository.get_contents(province_location)
@@ -515,6 +569,6 @@ if __name__ == '__main__':
     timepoints = get_timeseries_points()
     print(push_data_to_influx(timepoints))
 
-    data = get_data("csse_covid_19_data/csse_covid_19_daily_reports/")
+    data = get_data("csse_covid_19_data/csse_covid_19_daily_reports")
 
     print(push_data_to_influx(data))
